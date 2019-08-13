66b44f59d7191b64b413b2f0b91ffe945a422546
Rui Okada <okada.rui@gmail.com>
2019-08-12 21:18:00 -0700

    Initial commit

---
+++ sw.js
@@ -0,0 +1,35 @@
+const CACHE_URL = '/app_already_installed';
+
+self.addEventListener('activate', event => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', function (event) {
+  const {
+    request,
+    request: {
+      url,
+      method,
+    },
+  } = event;
+  if (url.match(CACHE_URL)) {
+    if (method === 'POST') {
+      request.json().then(body => {
+        caches.open(CACHE_URL).then(function (cache) {
+          cache.put(CACHE_URL, new Response(JSON.stringify(body)));
+        });
+      });
+      return new Response('{}');
+    } else {
+      event.respondWith(
+        caches.open(CACHE_URL).then(function (cache) {
+          return cache.match(CACHE_URL).then(function (response) {
+            return response || new Response('{}');;
+          }) || new Response('{}');
+        })
+      );
+    }
+  } else {
+    return event;
+  }
+});
